2942427e814f2189abd6f09e1b85dd6cab21163d
shyam-3045 <s.m.shyam45@gmail.com>
2025-11-20 12:08:53 +0530

    cleanup

--- Backend/server.js
+++ Backend/server.js
@@ -98,5 +98,5 @@ app.delete("/todo/:id",async(req,res)=>
 )
 app.listen(PORT,()=>
 {
-    console.log(`Server is running at : http://localhost:${PORT}`)
+    console.log(`Server is at : http://localhost:${PORT}`)
 })
