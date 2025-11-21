cd13eff38203c9c8e7b193db1f24653160212687
shyam-3045 <s.m.shyam45@gmail.com>
2025-11-21 13:18:29 +0530

    final fix

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose")
 const dotenv=require("dotenv").config()
 const cors=require("cors")
 
-const PORT=process.env.PORT ||3000;
+const PORT=process.env.PORT ||5000;
 
 
 app.use(express.json())
